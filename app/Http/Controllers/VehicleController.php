490892936cf1a2d192d58579ea72c1c1555d8e12
Moutlou <dhmm@dhmm.test>
2017-04-19 12:30:11 +0300

    Index and Show methods implemented

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -4,6 +4,7 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
+use App\Vehicle;
 
 class VehicleController extends Controller {
 
@@ -14,7 +15,8 @@ class VehicleController extends Controller {
 	 */
 	public function index()
 	{
-		return "I'm in index";
+		$vehicles = Vehicle::all();
+		return response()->json(['data' => $vehicles], 200);
 	}
 
 	
